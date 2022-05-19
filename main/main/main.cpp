73d7802f3b3960e0553789eabfcd16160258ced1
Kacper Horowski <79279375+GhostSiC@users.noreply.github.com>
2022-05-19 23:48:02 +0200

    Update main.cpp

--- main/main/main.cpp
+++ main/main/main.cpp
@@ -1,6 +1,25 @@
 ﻿#include <iostream>
+#include <algorithm>
+
+int solution(vector<int> &A) {
+
+    vector<int> answer;
+    vector<int>::iterator it;
+
+    for(auto& i : A){
+        it = find(answer.begin(), answer.end(), i);
+        if(it != answer.end())
+            answer.erase(it);
+        else
+            answer.push_back(i);
+    }
+
+    return answer.front();
+
+}
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
+
